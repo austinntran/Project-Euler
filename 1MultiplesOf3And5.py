94126e2f7107fccbaca14f9369c735401146a18a
austinntran <64512371+austinntran@users.noreply.github.com>
2020-05-11 18:05:19 -0400

    Add files via upload

---
+++ 1MultiplesOf3And5.py
@@ -0,0 +1,14 @@
+def main():
+    #variable to total result
+    sum = 0
+
+    #loop iterates from 0-999 inclusive
+    for i in range(1000):
+
+        #increment only if a multiple of 3 or 5
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+
+    print(sum)
+
+main()
