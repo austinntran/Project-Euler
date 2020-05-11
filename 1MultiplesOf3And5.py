d985b581e7a6484529c6b48a816ff9cf2af7f01b
Austin Tran <64512371+austinntran@users.noreply.github.com>
2020-05-11 18:20:07 -0400

    Update 1MultiplesOf3And5.py

--- 1MultiplesOf3And5.py
+++ 1MultiplesOf3And5.py
@@ -5,7 +5,7 @@ def main():
     #loop iterates from 0-999 inclusive
     for i in range(1000):
 
-        #increment only if a multiple of 3 or 5
+        #add to total only if a multiple of 3 or 5
         if i % 3 == 0 or i % 5 == 0:
             sum += i
 
